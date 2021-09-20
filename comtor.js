af3e537d189f1dae66972a648580c7c02112b2ef
Jaime Uriel Torres Serrano <juriel@comtor.net>
2021-09-20 15:50:30 -0500

    Avances

--- comtor.js
+++ comtor.js
@@ -2,13 +2,37 @@ const comtor =  {
     hello: function(){
         console.log("Hello World");
     },
-
     node2object: function(node){
+        pojo = {};
+        elements = node.getElementsByTagName("input");
 
+        for (const input of elements) {
+            if (input.type != "submit"){
+                pojo[input.name] = input.value;
+            }
+        }
+
+        selectss = node.getElementsByTagName("select");
+        for (const selects of selectss) {
+            pojo[input.name] = input.value;
+        }
+        texareas = node.getElementsByTagName("texarea");
+        return pojo;
     }
-}
+    ,
+    submitListener: function(evt){
+        evt.preventDefault();
+        console.log("TEST 2");
+        console.log(evt);
+        
+        return false;        
+    }
+};
+
 
 
 window.onload = function (){
     comtor.hello();
+    document.getElementById("myform").addEventListener("submit",comtor.submitListener);
+
 }
