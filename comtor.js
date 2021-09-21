0bf19fd46317731c2ce77b9fb186de4dcba402b4
Jaime Uriel Torres Serrano <juriel@comtor.net>
2021-09-20 19:57:51 -0500

    more advances

--- comtor.js
+++ comtor.js
@@ -1,44 +1,56 @@
-const comtor =  {
-    hello: function(){
+const comtor = {
+    hello: function () {
         console.log("Hello World");
     },
-    node2object: function(node){
+    node2object: function (node) {
         pojo = {};
-        elements = node.getElementsByTagName("input");
-
-        for (const input of elements) {
-            if (input.type != "submit"){
-                pojo[input.name] = input.value;
-            }        
+        elements = [];
+        inputs = node.getElementsByTagName("input");
+        selects = node.getElementsByTagName("select");
+        texareas = node.getElementsByTagName("textarea");
+        for (const ele of inputs) {
+            elements.push(ele);
+        }
+        for (const ele of selects) {
+            elements.push(ele);
+        }
+        for (const ele of texareas) {
+            elements.push(ele);
+        }
+        for (const ele of elements) {
+            
+            if (ele.type != "submit") {
+                if (!ele.disabled) {
+                    if (ele.name) {
+                        pojo[ele.name] = ele.value;
+                    }
+                    else if (ele.id) {
+                        pojo[ele.id] = ele.value;
+                    }
+                }
+            }
         }
 
-        selectss = node.getElementsByTagName("select");
-        for (const sele of selectss) {
-            pojo[sele.name] = sele.value;
-        }
-        texareas = node.getElementsByTagName("texarea");
         return pojo;
     }
     ,
-    submitListener: function(evt){
+    formSubmitListener: function (evt) {
         evt.preventDefault();
-        console.log(evt);   
+        console.log(evt);
         console.log("Target");
         console.log(evt.target);
         pojo = comtor.node2object(evt.target);
         if (evt.submitter instanceof HTMLInputElement) {
             pojo[evt.submitter.name] = evt.submitter.value;
         }
-
         console.log(pojo);
-        return false;        
+        return false;
     }
 };
 
 
-
-window.onload = function (){
+window.onload = function () {
     comtor.hello();
-    document.getElementById("myform").addEventListener("submit",comtor.submitListener);
+    document.getElementById("myform").addEventListener("submit", comtor.formSubmitListener);
 
 }
